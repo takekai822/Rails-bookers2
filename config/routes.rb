198c59b09c84b55af08dacda2765feba6e33a86d
takekai822 <kaikisoccer78@gmail.com>
2022-05-07 23:39:26 +0000

    [Add]guestlogin

--- config/routes.rb
+++ config/routes.rb
@@ -26,5 +26,9 @@ Rails.application.routes.draw do
   get "category_search" => "category_searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :chats, only: [:show, :create]
+  
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 
 end
